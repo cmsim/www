91c4ddfe1d7cd07f40c6c9e019dd2ef653081d3c
plain <dxhuii@qq.com>
2021-12-16 02:38:34 +0800

    完善sw

--- components/meta.tsx
+++ components/meta.tsx
@@ -2,10 +2,10 @@ import Head from 'next/head'
 
 const Meta = () => (
   <Head>
+    <meta charSet='utf-8' />
     <title>藏、藏书、藏动漫、藏网站、藏any</title>
     <meta name='description' content='一个可以收藏任何东西的网站' />
     <meta name='keyword' content='藏,藏书,藏动漫,藏网站,藏any' />
-    <meta charSet='utf-8' />
     <meta name='mobile-web-app-capable' content='yes' />
     <meta name='apple-mobile-web-app-capable' content='yes' />
     <meta
@@ -14,7 +14,7 @@ const Meta = () => (
     />
     <meta name='apple-mobile-web-app-title' content='藏' />
     <meta name='application-name' content='藏' />
-    <meta name='theme-color' content='#1d2020' />
+    <meta name='theme-color' content='#ffffff' />
     <meta
       name='viewport'
       content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
 module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  },
   future: {
     webpack5: true
   },

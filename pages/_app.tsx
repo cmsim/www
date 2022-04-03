6cab1858069046d8b580c57eab90591ad181d5df
丁效辉 <xiaohui.ding@weimob.com>
2022-04-03 23:04:37 +0800

    更新接口地址

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import '../styles/globals.css'
 import qs from 'query-string'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const prefix = 'http://127.0.0.1:7001/api/'
+  const prefix = 'https://d.vv.chat/api/'
   return (
     <ThemeProvider attribute='class' defaultTheme='light' disableTransitionOnChange>
       <Meta />

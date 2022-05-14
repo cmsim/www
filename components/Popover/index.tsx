15a729b48289e836747ad85482b28c216e4c3245
丁效辉 <xiaohui.ding@weimob.com>
2022-05-14 19:18:14 +0800

    更新

--- components/Popover/index.tsx
+++ components/Popover/index.tsx
@@ -1,4 +1,4 @@
-import { Popover as RePopover, Transition } from '@headlessui/react'
+import { Popover, Transition } from '@headlessui/react'
 import { FC, Fragment, ReactNode } from 'react'
 
 interface IPopover {
@@ -7,30 +7,20 @@ interface IPopover {
   className?: string
 }
 
-const Popover: FC<IPopover> = props => {
+const PopoverComponent: FC<IPopover> = props => {
   const { children, content, className = 'top-12 max-w-sm' } = props
   return (
-    <RePopover as={Fragment}>
+    <Popover as={Fragment}>
       {({ open }) => (
         <>
-          <RePopover.Button as={Fragment}>{children(open)}</RePopover.Button>
-          <Transition
-            show={open}
-            as={Fragment}
-            enter='transition ease-out duration-200'
-            enterFrom='opacity-0 translate-y-1'
-            enterTo='opacity-100 translate-y-0'
-            leave='transition ease-in duration-150'
-            leaveFrom='opacity-100 translate-y-0'
-            leaveTo='opacity-0 translate-y-1'>
-            <RePopover.Panel className={`absolute z-10 px-4 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0 ${className}`}>
-              {content}
-            </RePopover.Panel>
-          </Transition>
+          <Popover.Button as={Fragment}>{children(open)}</Popover.Button>
+          <Popover.Panel className={`absolute z-10 px-4 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0 ${className}`}>
+            {content}
+          </Popover.Panel>
         </>
       )}
-    </RePopover>
+    </Popover>
   )
 }
 
-export default Popover
+export default PopoverComponent
---
+++ pages/subject/index.tsx
@@ -0,0 +1,3 @@
+export default function Example() {
+  return <div className='fixed top-16 w-full max-w-sm px-4'>subject</div>
+}
